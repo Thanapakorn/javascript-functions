f2987c220067de58abd2177e1dfc90991281b4e7
thanapakorn <thanapakorn001@gmail.com>
2024-10-03 12:21:09 +0700

    JavaScript Functions Assignment

--- ex-1.js
+++ ex-1.js
@@ -1,1 +1,13 @@
 // Start coding here
+function add(a, b) {
+  return Number(a) + Number(b);
+}
+function subtract(a, b) {
+  return Number(a) - Number(b);
+}
+function multiply(a, b) {
+  return Number(a) * Number(b);
+}
+function divide(a, b) {
+  return Number(a) / Number(b);
+}
--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,20 @@
 // Start coding here
-let calculator = {};
+let calculator = {
+  add: (a, b) => {
+    return Number(a) + Number(b);
+  },
+  subtract: (a, b) => {
+    return Number(a) - Number(b);
+  },
+  multiply: (a, b) => {
+    return Number(a) * Number(b);
+  },
+  divide: (a, b) => {
+    return Number(a) / Number(b);
+  },
+};
+
+let addResult = calculator.add(10, 20);
+console.log(addResult);
+let divideResult = calculator.divide(3000, 10);
+console.log(divideResult);
