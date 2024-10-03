2fb8a2bf33943932015394190f5975bfd0394590
FSD7-Boy <boyzasad01@gmail.com>
2024-10-03 12:22:12 +0700

    feat:assignment javascript function

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,28 @@
 // Start coding here
-let calculator = {};
+let add = (a, b) => {
+  return a + b;
+};
+
+let subtract = (a, b) => {
+  return a - b;
+};
+
+let multiply = (a, b) => {
+  return a * b;
+};
+
+let divide = (a, b) => {
+  return a / b;
+};
+
+let calculator = {
+  add: add,
+  subtract: subtract,
+  multiply: multiply,
+  divide: divide,
+};
+
+console.log(calculator.add(10, 5));
+console.log(calculator.subtract(10, 5));
+console.log(calculator.multiply(10, 5));
+console.log(calculator.divide(10, 5));
